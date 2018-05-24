3afdc8a01494ca6037437f1899bd1d35accffec8
Gleb Petrakov <g.petrakov@innopolis.ru>
2018-05-24 11:42:55 +0300

    Fully implemented binarizer

--- assignments/ha-0/binarize.c
+++ assignments/ha-0/binarize.c
@@ -3,8 +3,8 @@
 void binarize_unsigned(unsigned long long x) {
 
     // If exceeds max possible value, say about overflow and exit
-    if (x > (INT64_MAX)) {
-        printf("Overflow");
+    if (x > ((INT64_MAX << 1) + 1)) {
+        printf("Overflow\n");
         return;
     }
 
@@ -12,7 +12,7 @@ void binarize_unsigned(unsigned long long x) {
     unsigned short sep = 1;     // Space separator counter
 
     // Set number of bit blocks based on input value
-    unsigned int boundary = x > INT32_MAX ? 62 : x > INT16_MAX ? 30 : x > INT8_MAX ? 14 : 6;
+    unsigned int boundary = x > INT64_MAX ? 126 : x > INT32_MAX ? 62 : x > INT16_MAX ? 30 : x > INT8_MAX ? 14 : 6;
 
     // Leading sign zero
     printf("0");
@@ -39,8 +39,58 @@ void binarize_unsigned(unsigned long long x) {
 }
 
 void binarize_signed(signed long long x) {
+
+    // Check for overflow
+    signed long long modulo = llabs(x);
+    if (modulo > INT64_MAX) {
+        printf("Overflow\n");
+        return;
+    }
+
+    // No need to perform more complex algorithm to positive numbers
     if (x >= 0) {
         binarize_unsigned(x);
         return;
     }
+
+    // Set up sign bit
+    unsigned int sign = x >= 0 ? 0 : 1;
+
+    // Set up bound for number
+    unsigned int bound = modulo > INT32_MAX ? 64 : modulo > INT16_MAX ? 32 : modulo > INT8_MAX ? 16 : 8;
+    unsigned int binary[bound];
+
+    // Convert the modulo to binary in little-endian notation
+    for (int i = 0; i < bound - 1; ++i) {
+        binary[i] = (unsigned int) x % 2;
+        x = x >> 1;
+    }
+
+    // Compliment the number
+    for (int i = 0; i < bound - 1; ++i) {
+        binary[i] = binary[i] == 0 ? 1 : 0;
+    }
+
+    // Add 1
+    unsigned int carry = 1;
+    for (int i = 0; i < bound - 1; ++i) {
+        binary[i] = binary[i] ^ carry;
+        carry == binary[i] & carry;
+    }
+
+    // Print the number
+    unsigned int delimiter = 0;
+    binary[bound - 1] = sign;
+    for (int i = bound - 1; i >= 0; --i) {
+        if (delimiter == 8) {
+            printf(" ");
+            delimiter = 0;
+        }
+        ++delimiter;
+
+        printf("%i", binary[i]);
+    }
+
+    // New line
+    printf("\n");
 }
--- assignments/ha-0/test_binarize.c
+++ assignments/ha-0/test_binarize.c
@@ -6,5 +6,14 @@ int main() {
     binarize_unsigned(9223372036854775807);
     binarize_unsigned(9223372036854775808);
 
+    binarize_unsigned(18446744073709551615);
+
+    binarize_signed(-9223372036854775804);
+
+    unsigned long ul = 1608637542;
+    signed long sl = -1608637542;
+    binarize_unsigned(ul);
+    binarize_signed(sl);
+
     return 0;
 }
